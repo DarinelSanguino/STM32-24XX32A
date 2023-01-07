c32a3573d9e554615f513971bb80351a382720aa
DarinelSanguino <darinel-sanguino@hotmail.com>
2023-01-07 17:02:41 -0600

    Add comments

--- src/24XX32A.c
+++ src/24XX32A.c
@@ -1,6 +1,11 @@
 #include "24XX32A.h"
 #include <string.h>
 
+/**********************************************************************
+* @brief byte_write() writes one byte of data to memory
+* @param address Memory location to write
+* @param data Byte to be saved in memory
+**********************************************************************/
 void byte_write(uint16_t address, uint8_t data) {
 	if(address > _24XX32A_MAX_ADDR) return;
 	while(!i2c_is_available(_24XX32A_I2C, _24XX32A_I2C_ADDR));
@@ -11,6 +16,12 @@ void byte_write(uint16_t address, uint8_t data) {
 	i2c_transfer7(_24XX32A_I2C, _24XX32A_I2C_ADDR, wr_buffer, 3, NULL, 0);
 }
 
+/**********************************************************************
+* @brief page_write() writes up to 32 bytes of data to memory
+* @param address First memory location to write
+* @param data Pointer to array of bytes to be saved in memory
+* @param n Number of bytes to write
+**********************************************************************/
 void page_write(uint16_t address, uint8_t *data, uint8_t n) {
 	if(address + n > _24XX32A_MAX_ADDR) return;
 	while(n > 0) {
@@ -29,11 +40,20 @@ void page_write(uint16_t address, uint8_t *data, uint8_t n) {
 	}
 }
 
+/************************************************************************************
+* @brief current_read() reads one byte of data from memory address in address counter
+* @param data buffer to save read byte
+************************************************************************************/
 void current_read(uint8_t *data) {
 	while(!i2c_is_available(_24XX32A_I2C, _24XX32A_I2C_ADDR));
 	i2c_transfer7(_24XX32A_I2C, _24XX32A_I2C_ADDR, NULL, 0, data, 1);	
 }
 
+/*****************************************************************************
+* @brief random_read() reads bytes starting from specified memory address
+* @param data buffer to save read bytes
+* @param n Number of bytes to read
+*****************************************************************************/
 void random_read(uint16_t address, uint8_t *data, uint8_t n) {
 	if(address > _24XX32A_MAX_ADDR) return;
 	while(!i2c_is_available(_24XX32A_I2C, _24XX32A_I2C_ADDR));
